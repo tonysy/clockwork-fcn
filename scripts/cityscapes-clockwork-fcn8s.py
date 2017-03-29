ff56fc9a7e80c524e2b8d7af128a150413d9655b
tonysy <sy.zhangbuaa@gmail.com>
2017-03-29 11:15:19 +0800

    add time test

--- scripts/cityscapes-clockwork-fcn8s.py
+++ scripts/cityscapes-clockwork-fcn8s.py
@@ -5,7 +5,7 @@ import random
 import caffe
 from lib import run_net
 from lib import score_util
-
+import time
 from datasets.cityscapes import cityscapes
 random.seed(0xCAFFE)
 
@@ -26,18 +26,34 @@ split = 'val'
 label_frames = CS.list_label_frames(split)
 
 #
+time_list = []
 hist_perframe = np.zeros((n_cl, n_cl))
 for i, idx in enumerate(label_frames):
     if i % 100 == 0:
         print 'running {}/{}'.format(i, len(label_frames))
     city = idx.split('_')[0]
     # idx is city_shot_frame
+    t = time.time()
+    
     im = CS.load_image(split, city, idx)
+
+    preprocess_t = time.time() - t
+    t = time.time()
+
     out = run_net.segrun(net, CS.preprocess(im))
+
+    net_forward_t = time.time() - t
+    t = time.time()
+
     label = CS.load_label(split, city, idx)
     hist_perframe += score_util.fast_hist(label.flatten(), out.flatten(), n_cl)
+
+    post_t = time.time() - t
+
+    time_list.append([preprocess_t, net_forward_t, post_t])
 
 accP, cl_accP, mean_iuP, fw_iuP = score_util.get_scores(hist_perframe)
 print 'Oracle: Per frame'
 print 'acc\t\t cl acc\t\t mIU\t\t fwIU'
 print '{:f}\t {:f}\t {:f}\t {:f}\t'.format(100*accP, 100*cl_accP, 100*mean_iuP, 100*fw_iuP)
+time_data = np.array(time_list)
